7cbbbf7d034357572e40ae772649c75042bc0134
mvr <matthewvonrocketstein@gmail-dot-com>
2015-01-22 08:42:50 +0000

    minor changes

--- corkscrew/__init__.py
+++ corkscrew/__init__.py
@@ -4,8 +4,8 @@
 """
 from .version import __version__
 from .settings import Settings
-from .views import View, MultiView
-from .blueprint import BluePrint
-from .app import App
+from corkscrew.views import View, MultiView
+from corkscrew.blueprint import BluePrint
+from corkscrew.app import App
 
 __all__= [ App, Settings, View, MultiView, BluePrint ]
