abae8da670beca3e5cf179576d4ac3d76fa2262d
mvr <matthewvonrocketstein@gmail-dot-com>
2015-06-01 19:31:25 +0000

    simplify app abstraction

--- corkscrew/app.py
+++ corkscrew/app.py
@@ -2,13 +2,9 @@
 
     TODO: better name instead of overloading "app" again..
 """
-class App(object):
-    def __init__(self, name='No name',
-                 url = '/no_url_given_for_app',
-                 use_image=None,
-                 requires_unauth=False,
-                 requires_auth=False):
-        self.name, self.url = name, url
-        self.requires_auth = requires_auth
-        self.requires_unauth = requires_unauth
-        self.use_image = use_image
+import json
+import addict
+
+class App(addict.Dict):
+    def render_json(self):
+        return json.dumps(dict(self))
